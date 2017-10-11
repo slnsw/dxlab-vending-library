fb16c01cb3b558c55d303dfe37122cc97f00b9a2
Luke Dearnley <ldearnley@c02pv21sfy14.staff.sl.nsw.gov.au>
2017-10-12 10:48:21 +1100

    updated ShortURL array due to 3.5/3.8 printing error

--- src/app.js
+++ src/app.js
@@ -152,7 +152,7 @@ const shortURLs = {
   3.5: 'https://goo.gl/dM28Hz',
   3.6: 'https://goo.gl/xq6EBe',
   3.7: 'https://goo.gl/ap9gpn',
-  3.8: 'https://goo.gl/pKm6vz',
+  3.8: 'https://goo.gl/dM28Hz', // 'https://goo.gl/pKm6vz', // printing error - content for 3.5 is also in slot 3.8
   C1: 'https://goo.gl/oYjNsG',
   C2: 'https://goo.gl/wuBY4a',
   C3: 'https://goo.gl/xrMPfC',
